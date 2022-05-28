b54791da5fd797f0f04c7d18bd7106135956ce9e
MalikovIslam <99679471+MalikovIslam@users.noreply.github.com>
2022-05-28 18:29:10 +0600

    Initial commit

---
+++ app/src/main/java/com/example/recyclerview/Contact.java
@@ -0,0 +1,17 @@
+package com.example.recyclerview;
+
+public class Contact {
+    private String name,number;
+    public Contact(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+}
